bed50aecc8f064f3bc0867227c7971d46aab66b7
Leszek Grzanka <leszek.grzanka@gmail.com>
2022-12-07 10:03:02 +0100

    renaming polish variables to english to stick to good standards

--- main.py
+++ main.py
@@ -10,70 +10,70 @@ import numpy as np
 
 #cewka siodlowa
 #########parametry################
-dlugosc = 400.0
-rozwarcie = np.pi/2
-prad = 100.0
-promien = 110.0
+length_of_wire_mm = 400.0  # [mm]
+angle_rad = np.pi/2
+current_A = 100.0
+radius_mm = 110.0
 ##################################
 
-#przestrzenie liniowe (zbiory wartosci)
-ts_prosta = np.linspace(0.0, dlugosc, 100)
-ts_luk = np.linspace(0.0, rozwarcie, 100)
-print(ts_prosta)
-print(ts_luk)
+#przestrzenie liniowe (zbiory wartosci) length
+points_on_straight_part = np.linspace(0.0, length_of_wire_mm, 100)
+points_on_arc = np.linspace(0.0, angle_rad, 100)
+print(points_on_straight_part)
+print(points_on_arc)
 
 #listy/tablice punktow przewodu:
-angle = np.pi/2-rozwarcie/2
-vert1_1 = [(promien*np.cos(angle),promien*np.sin(np.pi/2-rozwarcie/2),0),
-        (promien*np.cos(np.pi/2-rozwarcie/2),promien*np.sin(np.pi/2-rozwarcie/2),dlugosc)]
+current_angle_rad = np.pi/2-angle_rad/2
+vert1_1 = [(radius_mm*np.cos(current_angle_rad),radius_mm*np.sin(np.pi/2-angle_rad/2),0),
+        (radius_mm*np.cos(np.pi/2-angle_rad/2),radius_mm*np.sin(np.pi/2-angle_rad/2),length_of_wire_mm)]
 
-vert1_2 = np.c_[promien*np.cos(np.pi/2-rozwarcie/2+ts_luk)+ts_luk*0,
-               promien*np.sin(np.pi/2-rozwarcie/2+ts_luk)+ts_luk*0, dlugosc+ts_luk*0]
+vert1_2 = np.c_[radius_mm*np.cos(np.pi/2-angle_rad/2+points_on_arc)+points_on_arc*0,
+               radius_mm*np.sin(np.pi/2-angle_rad/2+points_on_arc)+points_on_arc*0, length_of_wire_mm+points_on_arc*0]
 
-vert1_3 = [(-promien*np.cos(np.pi/2-rozwarcie/2),promien*np.sin(np.pi/2-rozwarcie/2),dlugosc),
-        (-promien*np.cos(np.pi/2-rozwarcie/2),promien*np.sin(np.pi/2-rozwarcie/2),0)]
+vert1_3 = [(-radius_mm*np.cos(np.pi/2-angle_rad/2),radius_mm*np.sin(np.pi/2-angle_rad/2),length_of_wire_mm),
+        (-radius_mm*np.cos(np.pi/2-angle_rad/2),radius_mm*np.sin(np.pi/2-angle_rad/2),0)]
 
-vert1_4 = np.c_[promien*np.cos(np.pi/2-rozwarcie/2+rozwarcie-ts_luk)+ts_luk*0,
-               promien*np.sin(np.pi/2-rozwarcie/2+rozwarcie-ts_luk)+ts_luk*0, ts_luk*0]
+vert1_4 = np.c_[radius_mm*np.cos(np.pi/2-angle_rad/2+angle_rad-points_on_arc)+points_on_arc*0,
+               radius_mm*np.sin(np.pi/2-angle_rad/2+angle_rad-points_on_arc)+points_on_arc*0, points_on_arc*0]
 #przeciwna strona:
 
-vert2_1 = [(promien*np.cos(np.pi/2-rozwarcie/2),-promien*np.sin(np.pi/2-rozwarcie/2),0),
-        (promien*np.cos(np.pi/2-rozwarcie/2),-promien*np.sin(np.pi/2-rozwarcie/2),dlugosc)]
+vert2_1 = [(radius_mm*np.cos(np.pi/2-angle_rad/2),-radius_mm*np.sin(np.pi/2-angle_rad/2),0),
+        (radius_mm*np.cos(np.pi/2-angle_rad/2),-radius_mm*np.sin(np.pi/2-angle_rad/2),length_of_wire_mm)]
 
-vert2_2 = np.c_[promien*np.cos(np.pi/2-rozwarcie/2+ts_luk)+ts_luk*0,
-               -promien*np.sin(np.pi/2-rozwarcie/2+ts_luk)+ts_luk*0, dlugosc+ts_luk*0]
+vert2_2 = np.c_[radius_mm*np.cos(np.pi/2-angle_rad/2+points_on_arc)+points_on_arc*0,
+               -radius_mm*np.sin(np.pi/2-angle_rad/2+points_on_arc)+points_on_arc*0, length_of_wire_mm+points_on_arc*0]
 
-vert2_3 = [(-promien*np.cos(np.pi/2-rozwarcie/2),-promien*np.sin(np.pi/2-rozwarcie/2),dlugosc),
-        (-promien*np.cos(np.pi/2-rozwarcie/2),-promien*np.sin(np.pi/2-rozwarcie/2),0)]
+vert2_3 = [(-radius_mm*np.cos(np.pi/2-angle_rad/2),-radius_mm*np.sin(np.pi/2-angle_rad/2),length_of_wire_mm),
+        (-radius_mm*np.cos(np.pi/2-angle_rad/2),-radius_mm*np.sin(np.pi/2-angle_rad/2),0)]
 
-vert2_4 = np.c_[promien*np.cos(np.pi/2-rozwarcie/2+rozwarcie-ts_luk)+ts_luk*0,
-               -promien*np.sin(np.pi/2-rozwarcie/2+rozwarcie-ts_luk)+ts_luk*0, ts_luk*0]
+vert2_4 = np.c_[radius_mm*np.cos(np.pi/2-angle_rad/2+angle_rad-points_on_arc)+points_on_arc*0,
+               -radius_mm*np.sin(np.pi/2-angle_rad/2+angle_rad-points_on_arc)+points_on_arc*0, points_on_arc*0]
 
 
 #utworzenie przewodow:
-drut1_1 = magpy.current.Line(current=prad, vertices=vert1_1)
-drut1_2 = magpy.current.Line(current=prad, vertices=vert1_2)
-drut1_3 = magpy.current.Line(current=prad, vertices=vert1_3)
-drut1_4 = magpy.current.Line(current=prad, vertices=vert1_4)
-drut2_1 = magpy.current.Line(current=prad, vertices=vert2_1)
-drut2_2 = magpy.current.Line(current=prad, vertices=vert2_2)
-drut2_3 = magpy.current.Line(current=prad, vertices=vert2_3)
-drut2_4 = magpy.current.Line(current=prad, vertices=vert2_4)
+wire1_1 = magpy.current.Line(current=current_A, vertices=vert1_1)
+wire1_2 = magpy.current.Line(current=current_A, vertices=vert1_2)
+wire1_3 = magpy.current.Line(current=current_A, vertices=vert1_3)
+wire1_4 = magpy.current.Line(current=current_A, vertices=vert1_4)
+wire2_1 = magpy.current.Line(current=current_A, vertices=vert2_1)
+wire2_2 = magpy.current.Line(current=current_A, vertices=vert2_2)
+wire2_3 = magpy.current.Line(current=current_A, vertices=vert2_3)
+wire2_4 = magpy.current.Line(current=current_A, vertices=vert2_4)
 
 
-cewka = magpy.Collection(drut1_1, drut1_2, drut1_3, drut1_4, drut2_1, drut2_2, drut2_3, drut2_4)
+coil = magpy.Collection(wire1_1, wire1_2, wire1_3, wire1_4, wire2_1, wire2_2, wire2_3, wire2_4)
 
-magpy.show(cewka)
+magpy.show(coil)
 
-#zapis do pliku
-f = open("cevque.txt",'w')
-f.writelines("dlugosc:"+str(dlugosc)+"mm rozwarcie:"+str(rozwarcie)+" prad:"+str(prad)+"A promien:"+str(promien)+"mm\n")
+# write data to file
+filename = "coil.txt"
+f = open(filename,'w')
+# write length of wire, angle, current, radius to file
+f.write(f"length = {length_of_wire_mm} mm, angle = {angle_rad} rad, I = {current_A} A, radius = {radius_mm} mm\n")
 for x in range (-150, 150, 2):
     for y in range (-150, 150, 2):
         for z in range (0, 400, 2):
-            fx = magpy.getB(cewka, [x,y,z])[0]
-            fy = magpy.getB(cewka, [x,y,z])[1]
-            fz = magpy.getB(cewka, [x,y,z])[2]
-            f.writelines(str(x)+" "+str(y)+" "+str(z)+" "+str(fx)+" "+str(fy)+" "+str(fz)+"\n");
+            fx, fy, fz = magpy.getB(coil, [x,y,z])
+            f.write(f"{x} {y} {z} {fx} {fy} {fz}\n")
 f.close()
 
